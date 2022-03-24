64f363cea347fa43f99e42eceff18e095015d16c
lazinator1994 <84203550+lazinator1994@users.noreply.github.com>
2022-03-23 21:24:12 -0700

    new practice

---
+++ beginner - lost without a map/mapbeginner.js
@@ -0,0 +1,9 @@
+//Given an array of integers, return a new array with each value doubled.
+
+
+
+const mapItem = function maps(x) {
+    const timesByTwo = x.map(y => y * 2)
+    return timesByTwo 
+}
+
